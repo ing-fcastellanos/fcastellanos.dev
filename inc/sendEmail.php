e5925c9d9e647507f226e5bb5cd02ee6f3a047fa
fcastellanosKreios <fcastellanos@kreios.mx>
2021-02-24 09:39:07 -0600

    fixes al portfolio

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'ing.fcastellanos@gmail.com';
 
 
 if($_POST) {
@@ -13,15 +13,15 @@ if($_POST) {
 
    // Check Name
 	if (strlen($name) < 2) {
-		$error['name'] = "Please enter your name.";
+		$error['name'] = "Ingresa tu nombre.";
 	}
 	// Check Email
 	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-		$error['email'] = "Please enter a valid email address.";
+		$error['email'] = "Ingresa una direccion de correo válida.";
 	}
 	// Check Message
 	if (strlen($contact_message) < 15) {
-		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+		$error['message'] = "Ingresa un mensaje, este debe de tener por lo menos 15 caracteres";
 	}
    // Subject
 	if ($subject == '') { $subject = "Contact Form Submission"; }
